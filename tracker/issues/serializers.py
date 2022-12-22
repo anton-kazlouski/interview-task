725e76c1d435747d18bfcad215fcf66ff9df4d72
Anton Kozlovsky <anton.kozlovsky@solbeg.com>
2022-12-22 10:32:31 +0100

    TASK-2: Add DynamicFieldsMixin

--- tracker/issues/serializers.py
+++ tracker/issues/serializers.py
@@ -1,14 +1,22 @@
-from datetime import datetime, timedelta
-
 from rest_framework import serializers
 from .models import Issue
 
-from django.core.mail import send_mail
+
+class DynamicFieldsMixin:
+    def __init__(self, *args, **kwargs):
+        fields = kwargs.pop('fields', None)
+
+        if fields is not None:
+            allowed = set(fields)
+            existing = set(self.fields)
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
 
 
-class IssueSerializer(serializers.ModelSerializer):
-
-
-	class Meta:
-		model = Issue
-		fields = '__all__'
+class IssueSerializer(
+    serializers.ModelSerializer,
+    DynamicFieldsMixin,
+):
+    class Meta:
+        model = Issue
+        fields = '__all__'
--- tracker/issues/views.py
+++ tracker/issues/views.py
@@ -1,9 +1,29 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
 
 from .models import Issue
 from .serializers import IssueSerializer
 
 
 class IssueViewSet(ModelViewSet):
-	queryset = Issue.objects.all()
-	serializer_class = IssueSerializer
+    queryset = Issue.objects.all()
+    serializer_class = IssueSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(
+                page,
+                many=True,
+                fields=('id', 'summary')
+            )
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(
+            queryset,
+            many=True,
+            fields=('id', 'summary'),
+        )
+        return Response(serializer.data)
